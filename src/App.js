d4ae48dad484d86b1c926ae288ba4e68b3e0b833
Lookforrachel <lookfor.rachel@gmail.com>
2022-05-10 13:53:25 -0400

    Add intro section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello! I'm Rachel</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Intro from "./components/intro/Intro";
+
+const App = () => {
+  return <div><Intro/></div>;
+};
 
 export default App;
