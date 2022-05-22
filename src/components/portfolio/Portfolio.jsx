58e4a04e47e1f9b1eab8b420dd49ca641102105d
Lookforrachel <lookfor.rachel@gmail.com>
2022-05-21 22:26:22 -0400

    Change image to hero image, put text in the middle, add navbar socials and page links to top. Need to remove hamburger and add topbar background colour when not in intro section.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Graphic Design</li>
+        <li>Websites</li>
+        <li>Mobile App</li>
+        <li>Miscellaneous</li>
+      </ul>
+      <div className="container"></div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Conjugation App</h3>
+        </div>
     </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,20 +1,35 @@
 import "./topbar.scss"
-import Favicon from "../../img/RY_favicon.png"
-import {Mail} from "@material-ui/icons"
+import Logo from "../../img/RY_favicon.png"
+import {Mail, GitHub, LinkedIn, Twitter} from "@material-ui/icons"
 
 export default function Topbar({menuOpen, setMenuOpen}) {
   return (
     <div className = {"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo"><img src={Favicon} alt=""/></a>
-          <div className="itemContainer">
-            <a href="mailto: lookfor.rachel@gmail.com"><Mail className="icon"/></a>
-            <span>lookfor.rachel@gmail.com</span>
-          </div>
-          
+          <a href="#intro" className="logo"><img src={Logo} alt=""/></a>
+          <div className="iconWrapper">
+            <div className="itemContainer">
+                <a href="mailto: lookfor.rachel@gmail.com"><Mail className="icon"/></a>
+                {/* <span>lookfor.rachel@gmail.com</span> */}
+            </div>
+            <div className="itemContainer">
+                <a href="https://github.com/lookforrachel"><GitHub className="icon"/></a>
+            </div>
+            <div className="itemContainer">
+                <a href="https://www.linkedin.com/in/lookforrachel/"><LinkedIn className="icon"/></a>
+            </div>
+            <div className="itemContainer">
+                <a href="https://twitter.com/lookforrachel"><Twitter className="icon"/></a>
+            </div>
+        </div>
         </div>
         <div className="right">
+          <div className="pagesWrapper">
+            <a href="#intro" className="pageLink">Home</a>
+            <a href="#portfolio" className="pageLink">Portfolio</a>
+            <a href="#contact" className="pageLink">Contact</a>
+          </div>
           <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
