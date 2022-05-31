3b79309684734b7d5e8edfb70a646b9a4b08452f
Lookforrachel <lookfor.rachel@gmail.com>
2022-05-31 21:18:23 +0800

    Make contact form functional

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,23 +1,52 @@
 import { useState } from "react";
-import "./contact.scss"
+// import React, { useRef} from 'react'
+import "./contact.scss";
+// import { send } from "emailjs-com";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 export default function Contact() {
-const [message, setMessage] = useState(false)
+const form = useRef();
+const [inputs, setInputs] = useState({});
 
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    setMessage(true)
-  }
+const handleChange = (event) => {
+  const name = event.target.name;
+  const value = event.target.value;
+  setInputs(values => ({...values, [name]: value}))
+}
+
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  console.log(process.env)
+    emailjs.sendForm(
+    `${process.env.REACT_APP_SERVICE_ID}`,
+    `${process.env.REACT_APP_TEMPLATE_ID}`,
+    form.current,
+    `${process.env.REACT_APP_PUBLIC_KEY}`
+  )
+  .then((response) => {
+    console.log('Message sent successfully', response.status, response.text)
+  })
+  .catch((error) => {
+    console.log('Failed', error)
+  })
+  alert(inputs);
+  setInputs('')
+}
   return (
     <div className = "contact" id="contact">
       <div className="left"></div>
       <div className="right">
           <h2>Get in touch!</h2>
-          <form onSubmit={handleSubmit}>
-              <input type="text" placeholder="Email"/>
-              <textarea placeholder="Message"></textarea>
+          <form ref={form} onSubmit={handleSubmit}>
+              <input onChange={handleChange} type="email" name="email" id="email" className="email" placeholder="Email" value={inputs.email || ""} required/>
+              <input onChange={handleChange} type="text" name="name" id="name" className="name" placeholder="Name" value={inputs.name || ""} required/>
+              <input onChange={handleChange} type="text" name="subject" id="subject" className="subject" placeholder="Subject" value={inputs.subject || ""} required/>
+              <textarea onChange={handleChange} type="message" name="message" id="message" className="message" placeholder="Message" value={inputs.message || ""} required></textarea>
               <button type="submit">Send</button>
-              {message && <span>Thanks!</span>}
+              {/* {inputs.message && <span>Thanks!</span>} */}
           </form>
       </div>
     </div>
