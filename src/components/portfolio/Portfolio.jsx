d3a8c7cf075d0a0b35b04fde89bf23ef32d23d84
Lookforrachel <lookfor.rachel@gmail.com>
2022-07-23 22:12:26 -0400

    Adds graphic design links to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -67,11 +67,12 @@ export default function Portfolio() {
       </ul>
       <div className="container">
         {data.map(d=>(
-          <div className="item">
-            <img src={d.img} 
-            alt="" />
-            <h3>{d.title}</h3>
-          </div>
+          <a href={d.link} target="_blank" rel="noreferrer noopener">
+            <div className="item">                        
+              <img src={d.img} alt="" />
+              <h3>{d.title}</h3>
+            </div>
+          </a>
           ))}
         </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,26 +1,42 @@
 export const featuredPortfolio = [
     {
         id: 1,
-        title: "Social Media App",
+        title: "Conjugamer App",
         img: "",
+        link: ""
     },
     {
         id: 2,
         title: "Social Media App",
         img: "",
+        link: ""
     },
 ]
 
 export const graphicsPortfolio = [
     {
         id: 1,
-        title: "Graphic Design",
+        title: "Escapea",
         img: "",
+        link: "https://www.behance.net/gallery/148835999/Escapea"
     },
     {
         id: 2,
-        title: "Graphic Design",
+        title: "Morning Person",
         img: "",
+        link: "https://www.behance.net/gallery/148835909/Not-Much-of-a-Morning-Person"
+    },
+    {
+        id: 3,
+        title: "Christmas Card",
+        img: "",
+        link: "https://www.behance.net/gallery/148835879/Christmas-Card"
+    },
+    {
+        id: 4,
+        title: "Arts Crawl Poster",
+        img: "",
+        link: "https://www.behance.net/gallery/148835849/Arts-Crawl-Poster"
     },
 ]
 
@@ -29,11 +45,13 @@ export const webPortfolio = [
         id: 1,
         title: "Website",
         img: "",
+        link: ""
     },
     {
         id: 2,
         title: "Website",
         img: "",
+        link: ""
     },
 ]
 
@@ -42,11 +60,13 @@ export const devPortfolio = [
         id: 1,
         title: "Conjugation App",
         img: "",
+        link: ""
     },
     {
         id: 2,
         title: "Midi Controller",
         img: "",
+        link: ""
     },
 ]
 
@@ -55,10 +75,12 @@ export const miscPortfolio = [
         id: 1,
         title: "Misc",
         img: "",
+        link: ""
     },
     {
         id: 2,
         title: "Misc",
         img: "",
+        link: ""
     },
 ]
